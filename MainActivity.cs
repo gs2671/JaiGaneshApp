35c86bf12a7c8ab13299bc6d347b82634ad5ecae
Guruprasad Srinivasamurthy <gs2671@nyu.edu>
2017-02-21 19:53:15 -0500

    My first commit

---
+++ MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace JaiGaneshApp
+{
+    [Activity(Label = "JaiGaneshApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
